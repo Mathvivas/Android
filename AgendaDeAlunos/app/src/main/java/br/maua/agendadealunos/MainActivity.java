fb889f3221f33c46107224fd2b3afa9606208724
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-07 12:10:36 -0300

    Criação do Layout activity_main

--- AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
@@ -1,6 +1,17 @@
 package br.maua.agendadealunos;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView aluno = new TextView(this);
+        aluno.setText("Matheus Lopes Vivas");
+        setContentView(aluno);
+    }
 }
