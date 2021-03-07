79b2bdcce01a0347d9f86e66239baf3fc0ecdff5
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-07 14:09:09 -0300

    Update

--- AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
@@ -2,16 +2,25 @@ package br.maua.agendadealunos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Matheus Lopes Vivas");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Matheus", "Fran"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        // this representa essa MainActivity
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
