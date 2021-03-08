8301169123abb4541da90e5d7ee5d357907cc988
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-08 10:11:59 -0300

    Nova Activity

---
+++ AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,13 @@
+package br.maua.agendadealunos.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
