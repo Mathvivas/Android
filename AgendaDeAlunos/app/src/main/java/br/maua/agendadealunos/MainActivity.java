98e0e9337b11359b821a431223498fac9e689658
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-08 10:01:34 -0300

    Adição de Título no App

--- AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/MainActivity.java
@@ -1,23 +1,22 @@
 package br.maua.agendadealunos;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Matheus", "Fran"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         // this representa essa MainActivity
