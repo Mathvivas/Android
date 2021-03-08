2ea06471f34175f9f5ef7d2b11767dd1e2e0332c
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-08 10:54:57 -0300

    Update Activity do Formulário

--- AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/ui/activity/FormularioAlunoActivity.java
+++ AgendaDeAlunos/app/src/main/java/br/maua/agendadealunos/ui/activity/FormularioAlunoActivity.java
@@ -1,5 +1,9 @@
 package br.maua.agendadealunos.ui.activity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import br.maua.agendadealunos.R;
@@ -11,5 +15,19 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Cadastro de Alunos");
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+            }
+        });
     }
 }
